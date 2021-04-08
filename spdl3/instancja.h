92ceaf0311f109dc84b48087706262f1cce4b794
Kaja <80481808+GreekYogurtPowder@users.noreply.github.com>
2021-04-08 16:57:22 +0200

    Initial commit

---
+++ spdl3/instancja.h
@@ -0,0 +1,34 @@
+#ifndef INSTANCJA_H
+#define INSTANCJA_H
+
+#include "zadanie.h"
+#include "RandomNumberGenerator.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class instancja
+{
+public:
+	vector<zadanie> lista_zadan;
+	long ilosc_zadan;
+	long ilosc_operacji;
+
+	instancja();
+	void wypiszTabele();
+	
+	void wypiszPi();
+	void wypiszPi(vector<long> pi);
+
+	void funkcja_oceny();
+
+	/*
+	void calculate();
+	void calculate(vector<long> pi);
+	vector<long> Schrage();
+	*/
+};
+
+#endif
